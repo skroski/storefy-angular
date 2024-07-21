9c88854339255d3f3f89ea3415b82fff1d3fd0f0
Daniel Ribeiro Skroski <daniel@duplod.com.br>
2024-07-21 09:03:49 -0300

    Primeiro commit Aplicação Angular Migrada

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './shared/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  styles: [],
+  template: `
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+    `,
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent]
+})
+export class AppComponent {
+  title = 'angular-shop';
+}
